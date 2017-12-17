df490629468df49f0372ed754fb6fdf313f3930b
gorkaberzal <gorka.berzal@gmail.com>
2017-12-17 09:39:20 +0100

    Cleaning up some comments in Team.java

--- com/teamtreehouse/Team.java
+++ com/teamtreehouse/Team.java
@@ -1,28 +1,8 @@
 package com.teamtreehouse;
 
-/*import com.teamtreehouse.model.Player;
-import com.teamtreehouse.model.Players;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Map;
-import java.util.Set;
-import java.util.List;
-import java.util.HashMap;
-import java.util.TreeSet;*/
-
 public class Team implements Comparable<Team>{
   private String mTeamName;
   private String mCoach;
-//  private Team mTeam;
-//  private Teams mTeams;
-  
-  //Prompter prompter = new Prompter();
-  
-  //private BufferedReader mReader;
-  
- // private Map<String, String> mMenu;
   
   
   public Team (String teamName, String coach) {
